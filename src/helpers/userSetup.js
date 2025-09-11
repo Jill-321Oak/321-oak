cef73aa78fc4a869f39e89abe52209be1372f494
Jill-321Oak <jill@321Oak.com>
2025-09-11 17:23:02 -0400

    Update userSetup.js
    bug fix

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,15 +1,19 @@
- function userMarkdownSetup(md) {
--  // The md parameter stands for the markdown-it instance used throughout the site generator.
--  // Feel free to add any plugin you want here instead of /.eleventy.js
-+  // Allow raw HTML tags (e.g., <img>, <picture>) to render instead of being escaped.
-+  md.set({ html: true });
-+
-+  // If you want automatic link detection later, you can also enable:
-+  // md.set({ linkify: true });
- }
- function userEleventySetup(eleventyConfig) {
-   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
-   // Feel free to add any plugin you want here instead of /.eleventy.js
- }
- exports.userMarkdownSetup = userMarkdownSetup;
- exports.userEleventySetup = userEleventySetup;
+// src/helpers/userSetup.js
+
+function userMarkdownSetup(md) {
+  // Allow raw HTML tags (e.g., <img>, <picture>) to render instead of being escaped.
+  md.set({ html: true });
+
+  // Optional: automatically convert plain URLs into links.
+  // md.set({ linkify: true });
+}
+
+function userEleventySetup(eleventyConfig) {
+  // The eleventyConfig parameter stands for the config instantiated in /.eleventy.js.
+  // Add any custom Eleventy plugins or options here if needed.
+}
+
+module.exports = {
+  userMarkdownSetup,
+  userEleventySetup
+};
