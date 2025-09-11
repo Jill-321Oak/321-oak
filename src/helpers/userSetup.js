cf212d0bff260ce73a0628cd04e2d7185350cf25
Jill-321Oak <jill@321Oak.com>
2025-09-11 17:18:18 -0400

    fix(markdown): render raw <img>/<picture> by enabling HTML in markdown-it
    - Set md.set({ html: true }) in userMarkdownSetup so raw HTML isn’t escaped
    - Keeps existing Markdown behavior; no changes to Eleventy config required

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,10 +1,15 @@
-function userMarkdownSetup(md) {
-  // The md parameter stands for the markdown-it instance used throughout the site generator.
-  // Feel free to add any plugin you want here instead of /.eleventy.js
-}
-function userEleventySetup(eleventyConfig) {
-  // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
-  // Feel free to add any plugin you want here instead of /.eleventy.js
-}
-exports.userMarkdownSetup = userMarkdownSetup;
-exports.userEleventySetup = userEleventySetup;
+ function userMarkdownSetup(md) {
+-  // The md parameter stands for the markdown-it instance used throughout the site generator.
+-  // Feel free to add any plugin you want here instead of /.eleventy.js
++  // Allow raw HTML tags (e.g., <img>, <picture>) to render instead of being escaped.
++  md.set({ html: true });
++
++  // If you want automatic link detection later, you can also enable:
++  // md.set({ linkify: true });
+ }
+ function userEleventySetup(eleventyConfig) {
+   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
+   // Feel free to add any plugin you want here instead of /.eleventy.js
+ }
+ exports.userMarkdownSetup = userMarkdownSetup;
+ exports.userEleventySetup = userEleventySetup;
